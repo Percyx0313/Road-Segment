dcab7a93352d04f9b23a01fb0c111ef123672481
Sunny <sunnyhong91@gmail.com>
2024-05-27 02:39:22 +0800

    Add roadlane dataloader

--- jetbotData.py
+++ jetbotData.py
@@ -9,18 +9,36 @@ from sklearn.model_selection import train_test_split
 num_class = 4 # Road / Box shaped obstacle / Destination point / Background (Others) 
 new_h, new_w = 224, 224 # cropped image size
 flip_rate = 0.4
-img_dir = "./data/data_dataset_voc/JPEGImages"
-seg_img_dir = "./data/data_dataset_voc/SegmentationClassNpy"
+data_dirs = {
+    "labelme": {
+        "img_dir": "./data/labelme/data_dataset_voc/JPEGImages",
+        "seg_img_dir": "./data/labelme/data_dataset_voc/SegmentationClassNpy"
+    },
+    "roadlane": {
+        "img_dir": "./data/roadlane"
+    }
+}
 labels_file = "./data/data_dataset_voc/class_names.txt"
 
-def create_train_val_set(img_dir=img_dir, seg_dir=seg_img_dir):
-    X = list(sorted([os.path.join(img_dir, file) for file in os.listdir(img_dir)]))
-    y = list(sorted([os.path.join(seg_dir, file) for file in os.listdir(seg_dir)]))
+def create_train_val_set(data_dir, type="roadlane"):
+    if type == "labelme":
+        X = list(sorted([os.path.join(data_dir["img_dir"], file) for file in os.listdir(data_dir["img_dir"])]))
+        y = list(sorted([os.path.join(data_dir["seg_img_dir"], file) for file in os.listdir(data_dir["seg_img_dir"])]))
 
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, shuffle=True)
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, shuffle=True)  
+    elif type == "roadlane":
+        train_image_dir = os.path.join(data_dir["img_dir"], "train", "images")
+        train_seg_image_dir = os.path.join(data_dir["img_dir"], "train", "masks")
+        val_image_dir = os.path.join(data_dir["img_dir"], "val", "images")
+        val_seg_image_dir = os.path.join(data_dir["img_dir"], "val", "masks")
+
+        X_train = list(sorted([os.path.join(train_image_dir, file) for file in os.listdir(train_image_dir)]))  
+        y_train = list(sorted([os.path.join(train_seg_image_dir, file) for file in os.listdir(train_seg_image_dir)]))  
+        X_val = list(sorted([os.path.join(val_image_dir, file) for file in os.listdir(val_image_dir)])) 
+        y_val = list(sorted([os.path.join(val_seg_image_dir, file) for file in os.listdir(val_seg_image_dir)])) 
     return X_train, X_val, y_train, y_val
 
-class JetBotDataset(Dataset):
+class LabelmeDataset(Dataset):
     def __init__(self,
                  image_files, 
                  seg_img_files,
@@ -73,6 +91,83 @@ class JetBotDataset(Dataset):
 
         return {"img": img, "label": target}
     
+
+class RoadLaneDataset(Dataset):
+    def __init__(self,
+                 image_files, 
+                 seg_img_files,
+                 split="train"):
+        self.X = image_files
+        self.Y = seg_img_files
+        self.new_h = new_h
+        self.new_w = new_w
+        self.split = split
+        self.n_class = num_class
+        self.label_colors = {
+            0: [0,0,0],   # black -> background
+            1: [128,0,0],   # dark red -> road
+            2: [0,128,0],   # light green -> landmark solid
+            3: [128,128,0]  # green -> landmark dashed
+        }
+        self.colors = [[0,0,0],[128,0,0],[0,128,0],[128,128,0]]
+
+    def __len__(self):
+        return len(self.X)
+
+    def rgb_to_label_image(self, rgb_image):
+        # Create an empty label image with the same height and width as the input image
+        height, width, _ = rgb_image.shape
+        label_image = np.zeros((height, width), dtype=np.int32)
+        
+        # Loop through each color and assign the corresponding label
+        for label, color in self.label_colors.items():
+            # Create a mask for the current color
+            mask = np.all(rgb_image == color, axis=-1)
+            # Assign the label to the mask
+            label_image[mask] = label
+    
+        return label_image
+
+    def __getitem__(self, idx):
+        # open image data
+        img = Image.open(self.X[idx]).convert('RGB')
+        w, h = img.size
+        img = np.asarray(img)
+
+        # open segement image data 
+        img = Image.open(self.X[idx]).convert('RGB')
+        img = np.asarray(img)
+        seg_img = self.rgb_to_label_image(img)
+
+        # crop image and seg_img
+        if self.split == "train":
+            A_x_offset = np.int32(np.random.randint(0, w - self.new_w + 1, 1))[0]
+            A_y_offset = np.int32(np.random.randint(0, h - self.new_h + 1, 1))[0]
+        else:
+            A_x_offset = int((w - self.new_w)/2)
+            A_y_offset = int((h - self.new_h)/2)
+            
+        img = img[A_y_offset: A_y_offset + self.new_h, A_x_offset: A_x_offset + self.new_w] 
+        seg_img = seg_img[A_y_offset: A_y_offset + self.new_h, A_x_offset: A_x_offset + self.new_w]
+
+        # flip images and seg_img 
+        img = np.transpose(img, (2, 0, 1)) / 255.
+        if np.random.sample() < flip_rate and self.split == "train":
+            img = np.fliplr(img)
+            seg_img = np.fliplr(seg_img)
+
+        # create tensor
+        img = torch.from_numpy(img.copy()).float()
+        seg_img = torch.from_numpy(seg_img.copy()).long()
+
+        # create one-hot encoding tensor
+        h, w = seg_img.size()
+        target = torch.zeros(self.n_class, h, w)
+        for c in range(self.n_class):
+            target[c][seg_img == c] = 1
+
+        return {"img": img, "label": target}
+    
 def read_labels(labels_file=labels_file):
     label_map = {}
     with open(labels_file, 'r') as fp:
@@ -81,13 +176,20 @@ def read_labels(labels_file=labels_file):
     # {0: '_background_\n', 1: 'road\n', 2: 'obstacle', 3: 'destination'}
     return label_map
     
-def get_train_val_dataloader(batch_size, num_workers=0):
-    X_train, X_val, y_train, y_val = create_train_val_set()
+def get_train_val_dataloader(batch_size, num_workers=0, type='roadlane'):
+    train_set = None
+    val_set = None
+    data_dir = data_dirs[type]
+    X_train, X_val, y_train, y_val = create_train_val_set(data_dir, type)
 
     # train set enable data augumentation
-    train_set = JetBotDataset(X_train, y_train, 'train')
     # val set no data augumentation
-    val_set = JetBotDataset(X_val, y_val, 'val')
+    if type == "labelme":
+        train_set = LabelmeDataset(X_train, y_train, 'train')
+        val_set = LabelmeDataset(X_val, y_val, 'val')
+    elif type == "roadlane":
+        train_set = RoadLaneDataset(X_train, y_train, 'train')
+        val_set = RoadLaneDataset(X_val, y_val, 'val')
     
     dataset_size = {'train': train_set.__len__(), 'val': val_set.__len__()}
 
